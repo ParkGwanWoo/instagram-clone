91b0f7f3f74b5dd26fff890632d35449d1dc20c3
DevYeon <yeonkevin@icloud.com>
2018-07-19 10:23:29 +0900

    Customizing the Django Admin Site

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from instagram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,7 +11,7 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
     """ Image Model """
     file = models.ImageField()
@@ -17,14 +19,24 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
     """ Comment Model """
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
     image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
     """ Like Model """
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
     image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
