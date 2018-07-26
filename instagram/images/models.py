684bbb595a8f16e92a5faab035237894e07af374
DevYeon <yeonkevin@icloud.com>
2018-07-26 15:02:10 +0900

    bug fix

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -18,7 +18,7 @@ class Image(TimeStampedModel):
     location = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
-
+    
     def __str__(self):
         return '{} - {}'.format(self.location, self.caption)
 
@@ -27,7 +27,7 @@ class Comment(TimeStampedModel):
     """ Comment Model """
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name="comments")
 
     def __str__(self):
         return self.message
@@ -36,7 +36,7 @@ class Comment(TimeStampedModel):
 class Like(TimeStampedModel):
     """ Like Model """
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name="likes")
 
     def __str__(self):
         return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
