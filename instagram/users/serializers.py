f2bf9b4d648309b387a9dbcf9df708836868d980
DevYeon <yeonkevin@icloud.com>
2018-07-28 15:02:06 +0900

    following a user && unfllowing a user

--- instagram/users/serializers.py
+++ instagram/users/serializers.py
@@ -5,6 +5,7 @@ class ExploreUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = (
+            'id',
             'profile_image',
             'username',
             'name',
