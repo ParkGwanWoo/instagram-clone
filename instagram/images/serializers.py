7bc1db09073a4e945f8336a27de6b08531d31472
DevYeon <yeonkevin@icloud.com>
2018-07-28 12:57:30 +0900

    Commenting on an Image, Delete My Comment

--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -20,14 +20,14 @@ class FeedUserSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     
-    creator = FeedUserSerializer()
+    creator = FeedUserSerializer(read_only=True)
 
     class Meta:
         model = models.Comment
         fields = (
             'id',
             'message',
-            'creator'
+            'creator',
         )
 
 
