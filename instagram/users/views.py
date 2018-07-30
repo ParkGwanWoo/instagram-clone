e1009d6c7ecf910cabf37c32afb28a77c035f6c4
DevYeon <yeonkevin@icloud.com>
2018-07-30 18:46:15 +0900

    change the exploreSerializer name

--- instagram/users/views.py
+++ instagram/users/views.py
@@ -6,7 +6,7 @@ from . import models, serializers
 class ExploreUsers(APIView):
     def get(self, request, format=None):
         last_five = models.User.objects.all().order_by('-date_joined')[:5]
-        serializer = serializers.ExploreUserSerializer(last_five, many=True)
+        serializer = serializers.ListUserSerializer(last_five, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
