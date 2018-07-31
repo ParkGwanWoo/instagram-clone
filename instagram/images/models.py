f59bae22a19a8d7b4c2c2206c7d9c2a21fe534c7
DevYeon <yeonkevin@icloud.com>
2018-07-31 13:56:25 +0900

    install django-taggit & create search model

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from instagram.users import models as user_models
 from django.utils.encoding import python_2_unicode_compatible
+from taggit.managers import TaggableManager
+
 # Create your models here.
 
 @python_2_unicode_compatible
@@ -18,7 +20,8 @@ class Image(TimeStampedModel):
     location = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, related_name="images")
-
+    tags = TaggableManager()
+    
     @property
     def like_count(self):
         return self.likes.all().count()
