aab24e3b14168738c0d7c7e88f15b1f3cb0b107d
DevYeon <yeonkevin@icloud.com>
2018-08-04 16:03:53 +0900

    Getting the image likes

--- instagram/images/views.py
+++ instagram/images/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from . import models, serializers
+from instagram.users import models as user_models
+from instagram.users import serializers as user_serializers
 from instagram.notifications import views as notification_views
 
 class Feed(APIView):
@@ -29,6 +31,15 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+    def get(self, request, image_id, format=None):
+        likes = models.Like.objects.filter(image__id=image_id)
+        like_creators_ids = likes.values('creator_id')
+
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request, image_id, format=None):
         user = request.user
 
